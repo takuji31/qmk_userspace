a13bc9c2fd88268c6deb8ffc7fa33e315b07da51
Takuji Nishibayashi <takuji31@gmail.com>
2025-09-01 18:14:05 +0900

    update futaba keymap layouts and base firmware

--- keyboards/geek_rabb1t/futaba/keymaps/takuji31/config.h
+++ keyboards/geek_rabb1t/futaba/keymaps/takuji31/config.h
@@ -7,6 +7,7 @@
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 16
+// cannot increase because of EEPROM size
+#define DYNAMIC_KEYMAP_LAYER_COUNT 9
 #define VIAL_TAP_DANCE_ENTRIES 32
 #define TAPPING_TERM 180
